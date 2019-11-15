a68f6d227bfa673a33b2015202390d9a69af34fe
DCCA <dcca12@gmail.com>
2019-11-14 20:41:23 -0800

    add eslint, browsefy, rename and terser to gulp

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var daniel = 'daniel';
+
+const teste = 1;
+
+let juntar = daniel + teste;
+
+let juntar312 = daniel + teste + daniel;
+
+let junntaa = daniel + daniel + daniel;
+
+let dasDdsads = daniel;
