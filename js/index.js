6da3ebab48aaf835491eee32b65af8c976f2bad1
Daniel Andrade <dcca12@gmail.com>
2019-11-21 16:44:48 -0800

    add loader

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 import Card from './Cards.min.js';
 
+// Start of document ready function
 $( document ).ready(function() {
+// Input code inside
+
 
 let thereIsNews = false;
 const news = $('.news')
+const sections = $('.sections')
+const loader = $('.loader');
 const nytUrl = 'https://api.nytimes.com/svc/topstories/v2/'
 const endUrl = '.json?api-key=CLl9cdH4BgVqWucoFHZ3wdB1rsXAtoBt'
 const arrSection = [
@@ -41,10 +46,10 @@ start();
 function start(){
     // Create the options in the dropdown
     arrSection.forEach(element => {
-        $('.sections').append( '<option value="' + element + '">' + element + '</option>');
+        sections.append( '<option value="' + element + '">' + element + '</option>');
     });
     // Add the event listener
-    $('.sections').on('change', makeApiCall)
+    sections.on('change', makeApiCall)
 }
 
 function toggleClasses(){
@@ -55,7 +60,7 @@ function toggleClasses(){
 }
 
 function createNewsCards(jData, news){
-  for(let i = 0; i < 20 ; i++){
+  for(let i = 0; i < 12 ; i++){
     let newCard = new Card(jData.results[i].abstract, jData.results[i].url,jData.results[i].multimedia[4].url);
     newCard.create(news);
   }
@@ -63,20 +68,23 @@ function createNewsCards(jData, news){
 
 function makeApiCall(event){
   let section = event.target.value;
+  loader.toggle();
   $.ajax({
     method: 'GET',
     url: nytUrl + section + endUrl
   })
     .done(function(data) {
+      loader.toggle();
       let jData = data;
       if(!thereIsNews){
         toggleClasses();
       }
-      $('.news').text('');
+      news.text('');
       createNewsCards(jData, news);
       thereIsNews = true;
     })
     .fail(function() {
+      loader.toggle();
       console.log('FAIL!');
     });
   }
@@ -84,4 +92,5 @@ function makeApiCall(event){
 
 // End of document ready function
 });
+// Don't right code after this line
 
